59102adba498ff14888da7a0e0401ac25fb439f3
Pablo Crovetto <jpcc077@gmail.com>
2020-01-03 11:46:03 -0500

    Login tests

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,4 +1,39 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class LoginFormComponent extends Component {
+
+  @tracked
+  userId = '';
+
+  get isDisabled() {
+    return !this.userId;
+  }
+
+  loginAsUserWithId(val){
+    console.log('user', val);
+  }
+
+  /**
+   *
+   * @param {Event & {target: HTMLFormElement}} evt
+   */
+  @action
+  onLoginFormSubmit(evt) {
+    const { target } = evt;
+    const val = target.querySelector('select').value;
+    evt.preventDefault();
+    this.loginAsUserWithId(val);
+  }
+
+  /**
+   *
+   * @param {Event & {target: HTMLSelectElement}} evt
+   */
+  @action
+  onSelectChanged(evt) {
+    this.userId = evt.target.value;
+  }
+
 }
--- tests/integration/components/login-form-test.js
+++ tests/integration/components/login-form-test.js
@@ -1,26 +1,35 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, fillIn, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | login-form', function(hooks) {
+module('Integration | Component | login-form', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
     await render(hbs`<LoginForm />`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.deepEqual(
+      this.element.textContent
+      .trim()
+      .replace(/\s*\n+\s*/g, '\n')
+      .split('\n'),
+      [
+        "Login",
+        "Select a user",
+        "Testy Testerson",
+        "Sample McData"
+      ]
+    );
 
-    // Template block usage:
-    await render(hbs`
-      <LoginForm>
-        template block text
-      </LoginForm>
-    `);
+    let button = /** @type {HTMLInputElement} */(find('input[type="submit"]'));
+    assert.equal(button.disabled, true);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    await fillIn('select', '1')
+
+    assert.equal(button.disabled, false);
   });
 });
